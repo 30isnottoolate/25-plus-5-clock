e4f5e31a356a39628973a10694ec7ae9c51327e3
30isnottoolate <akos.varga.92@gmail.com>
2022-07-19 17:04:17 +0200

    Add resetState() method
        Defined default values, and added resetState() to app.

--- src/script.js
+++ src/script.js
@@ -2,20 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app_styles.css';
 
+const DEF_B_LENGTH = 5; // default Break Length in minutes
+const DEF_S_LENGTH = 25; // default Session Length in minutes
+const DEF_B_LEFT = 300; // default Break length in seconds
+const DEF_S_LEFT = 1500; // default Session length in seconds
+const DEF_MODE = "Session"; // default mode
+const DEF_ACTIVE = false; // default activity status
+
 class TwentyFivePlusFive extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      breakLength: 5,
-      sessionLength: 25,
-      breakLeft: 300,
-      sessionLeft: 1500,
-      mode: "Session",
-      active: false,
+      breakLength: DEF_B_LENGTH,
+      sessionLength: DEF_S_LENGTH,
+      breakLeft: DEF_B_LEFT,
+      sessionLeft: DEF_S_LEFT,
+      mode: DEF_MODE,
+      active: DEF_ACTIVE,
       ping: ""
     };
+
+    this.alarm = React.createRef();
+
+    this.resetState = this.resetState.bind(this);
   }
-  
+
+  resetState() {
+    this.setState({
+      breakLength: DEF_B_LENGTH,
+      sessionLength: DEF_S_LENGTH,
+      breakLeft: DEF_B_LEFT,
+      sessionLeft: DEF_S_LEFT,
+      mode: DEF_MODE,
+      active: DEF_ACTIVE,
+      ping: ""
+    });
+  }
+
   render() {
     return (
       <div id="app-container">
@@ -24,19 +47,19 @@ class TwentyFivePlusFive extends React.Component {
         <p id="session-label" className="length-label">Session Length</p>
         <button id="session-decrement" className="length-button">-</button>
         <button id="session-increment" className="length-button">+</button>
-        <p id="session-length" className="length-counter">25</p>
+        <p id="session-length" className="length-counter">{this.state.sessionLength}</p>
 
         <p id="break-label" className="length-label">Break Length</p>
         <button id="break-decrement" className="length-button">-</button>
         <button id="break-increment" className="length-button">+</button>
-        <p id="break-length" className="length-counter">5</p>
+        <p id="break-length" className="length-counter">{this.state.breakLength}</p>
 
         <p id="timer-label">SESSION/BREAK</p>
         <p id="time-left">25:00</p>
         <button id="start_stop" className="time-button">START</button>
-        <button id="reset" className="time-button">RESET</button>
+        <button id="reset" className="time-button" onClick={this.resetState}>RESET</button>
         
-        <audio id="alarm" preload="auto" ref={this.beep} src="https://sampleswap.org/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Alarm%20Sounds/212[kb]oscillating-flyby.wav.mp3"/>
+        <audio id="alarm" preload="auto" ref={this.alarm} src="https://sampleswap.org/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Alarm%20Sounds/212[kb]oscillating-flyby.wav.mp3"/>
       </div>
     );
   }
