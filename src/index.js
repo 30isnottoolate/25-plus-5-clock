4c4c3abbd96117814063369c16b8c7d66ca916c6
30isnottoolate <107811595+30isnottoolate@users.noreply.github.com>
2022-07-25 17:13:12 +0200

    Render TwentyFivePlusFive from index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './script.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TwentyFivePlusFive from './TwentyFivePlusFive';
+
+const container = ReactDOM.createRoot(document.getElementById('app-container'));
+container.render(
+  <React.StrictMode>
+    <TwentyFivePlusFive />
+  </React.StrictMode>
+);
